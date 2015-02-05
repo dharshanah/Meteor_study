27475c8a8ab4da75cd3566caec1be54aac81be06
dharshanah <dharshanah@gmail.com>
2015-02-05 17:53:17 +0530

    Latency Compensation Example : Pg 134

--- discover_meteor/client/templates/posts/post_submit.js
+++ discover_meteor/client/templates/posts/post_submit.js
@@ -5,7 +5,18 @@ Template.postSubmit.events ({
 			url : $(e.target).find('[name=url]').val(),
 			title : $(e.target).find('[name=title]').val()
 		}
-		post._id = Posts.insert(post);
-		Router.go('postPage',post);
+		Meteor.call('postInsert' ,post, function(error, response){
+			if(error){
+				return alert("Error " + error.reason);
+			}
+			if(response.postExists){
+				alert('Url already exist. Redirecting')
+			}
+			//Commented for the sake of demonstrating Latency Compensation
+			//Router.go('postPage',{_id : response._id});
+		});
+		//Temp Route
+		Router.go('postsList');
+		
 	}
 })
--- discover_meteor/lib/collections/posts.js
+++ discover_meteor/lib/collections/posts.js
@@ -1,8 +1,47 @@
 Posts = new Mongo.Collection('posts');
 
+/** This becomes irrelevant once we start using Meteor.methods
 Posts.allow({
 	insert : function(userId , doc){
 		return !! userId;
 	}
 	
+});
+**/
+
+Meteor.methods({
+   postInsert : function(postAttributes){
+   		check(Meteor.userId(), String);
+   		check(postAttributes ,{
+   			title : String,
+   			url : String
+   		});
+   		//This code check is to understand Latency Compensation
+   		if(Meteor.isServer){
+   			postAttributes.title += "(server)";
+   			Meteor._sleepForMs(5000);
+
+   		} else {
+   			postAttributes.title += "(client)";
+   		}
+   		var postWithSameUrl = Posts.findOne({url: postAttributes.url});
+   		if(postWithSameUrl){
+   			return {
+   				postExists: true,
+   				_id : postWithSameUrl._id
+   			};
+   		}
+   		var user = Meteor.user();
+   		var post = _.extend(postAttributes ,{
+   			userId : user._id,
+   			author : user.name,
+   			submitted : new Date()
+   		});
+
+   		var postId = Posts.insert(post);
+   		return {
+   			_id : postId
+   		};
+
+   }
 });
