283c445226b578c6469f1aa6e5c81a2b36aa5585
dharshanah <dharshanah@gmail.com>
2015-02-05 15:26:59 +0530

    Reactivity Completed . Creating Posts Pg 119

--- discover_meteor/lib/router.js
+++ discover_meteor/lib/router.js
@@ -18,7 +18,22 @@ Router.route('/posts/:_id',{
 	}
 });
 
+var requireLogin = function(){
+	if(!Meteor.user()){
+		if(Meteor.loggingIn()){
+			this.render(this.loadingTemplate);
+		}
+		else {
+			this.render('accessDenied');
+		}
+	}
+	else {
+		this.next();
+	}
+}
+Router.route('/submit' , {name : 'postSubmit'});
 //The following statement tells the router to show the notFound 404 for postPage template
 //in case there is no post with the _id param value in url. That is if the data function returns falsy
 //('null' or 'false' or 'undefined' or empty) object.
 Router.onBeforeAction('dataNotFound' , {only : 'postPage'})
+Router.onBeforeAction(requireLogin , {only : 'postSubmit'})
